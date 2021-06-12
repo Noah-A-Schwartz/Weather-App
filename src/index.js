304ba0f17efb67ac21b26904add87bfa91bcf7e5
Noah Schwartz <noah.amir.schwartz@gmail.com>
2021-06-12 18:52:14 -0400

    Removed form so page does not refresh when submitting information. Added a default city (Atlanta) to show data from.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { showWeather } from "./weather.js";
 
-document.forms[0].onsubmit = getWeatherCity;
+
+
+document.getElementsByTagName("button")[0].addEventListener("click", getWeatherCity);
+let city = "atlanta";
+getDefaultWeatherCity();
+
 
 async function getWeatherCity() {
     try {
-        let form = document.forms[0];
-        let city = form[0].value;
+        city = document.getElementById("city-input").value;
         let key = "249607106b0cfa8d5d5a26a0ef539b09";
         let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
         const response = await fetch(url);
@@ -16,6 +20,24 @@ async function getWeatherCity() {
     catch (err) {
         console.log(err);
     }
+    
 }
+async function getDefaultWeatherCity() {
+    try {
+        
+        let key = "249607106b0cfa8d5d5a26a0ef539b09";
+        let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data)
+        showWeather(data);
+    }
+    catch (err) {
+        console.log(err);
+    }
+    
+}
+
+
 
 
--- src/weather.js
+++ src/weather.js
@@ -1,10 +1,11 @@
 function showWeather(data){
     let weatherElements = document.getElementsByTagName("p");
-    weatherElements[0].innerText = "Temp: " + farenToKelvin(data.main.temp) + "°F";
-    weatherElements[1].innerText = "Feels Like: " + farenToKelvin(data.main.feels_like) + "°F";
-    weatherElements[2].innerText = "High: " + farenToKelvin(data.main.temp_max) + "°F";
-    weatherElements[3].innerText = "Low: " + farenToKelvin(data.main.temp_min) + "°F";
-    weatherElements[4].innerText = "Humidity: " + data.main.humidity + "%"
+    weatherElements[0].innerText = (data.name)
+    weatherElements[1].innerText = "Temp: " + farenToKelvin(data.main.temp) + "°F";
+    weatherElements[2].innerText = "Feels Like: " + farenToKelvin(data.main.feels_like) + "°F";
+    weatherElements[3].innerText = "High: " + farenToKelvin(data.main.temp_max) + "°F";
+    weatherElements[4].innerText = "Low: " + farenToKelvin(data.main.temp_min) + "°F";
+    weatherElements[5].innerText = "Humidity: " + data.main.humidity + "%"
 
 }
 
